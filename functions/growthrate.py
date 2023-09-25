4a1c50908be75c4e549613b1abc6979d25603ca9
lattia <lattia@udel.edu>
2023-09-25 12:00:23 -0400

    growth rate
    included growth rate as a power law of supersaturation

---
+++ functions/growthrate.py
@@ -0,0 +1,13 @@
+def crystal_growth(S, g, k_g): 
+    """
+    Description: Function that defines the crystal growth rate 
+
+    Inputs: 
+        S: supersaturation [M]
+        g: empirical power constant [dimensionless]
+        k_N: crystal growth constant [1/m^3 *s]
+    Outputs: 
+        B: birth rate [m/s]
+    """
+    G = k_g * S**g
+    return G
