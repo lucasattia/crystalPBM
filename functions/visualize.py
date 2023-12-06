de201f274347e294a11e5ddfe9ab57d23d80be5d
Jeremy Kline <jbkline@mit.edu>
2023-12-06 10:18:17 -0500

    updated visualization

--- functions/visualize.py
+++ functions/visualize.py
@@ -1,17 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def visualize(n,L,t_ind):
-    num_samples = 30  # To be replaced with int(n,L)/normalization_factor
+def visualize(n,L,t_ind, num_scale = 100000, dot_scale=4, alpha=0.7):
+    # num_samples = 10  # To be replaced with int(n,L)/normalization_factor
+    norm_factor = np.trapz(n[-1,:], L)
+    num_samples = int(num_scale*np.trapz(n[t_ind,:], L)/norm_factor)
+    # print(num_samples)
 
-    # Ensure num_samples is not greater than the length of the arrays
-    num_samples = min(num_samples, len(n[t_ind,:]))
-
-    # Generate indices for evenly spaced samples
-    indices = np.linspace(0, len(n[t_ind,:]) - 1, num_samples, dtype=int)
-
-    L_sampled = L[indices]
-
+    # assuming that L is uniform. The first entry is 0 and we dont want that,
+    # so let's plot the "upper end" of each bin instead by adding dL to all the lengths
+    dL = L[1]
+    L_sampled = dL + np.random.choice(L, num_samples, replace=True, p=n[t_ind,:]/np.sum(n[t_ind,:]))
     num_circles = len(L_sampled)
 
     x_min=0
@@ -24,17 +23,18 @@ def visualize(n,L,t_ind):
     y_coords = np.random.uniform(low=y_min, high=y_max, size=num_circles)  
 
     # Create a plot
+    
     fig, ax = plt.subplots()
+    ax.scatter(x_coords, y_coords, dot_scale*L_sampled, alpha=alpha)
 
     # Plot each circle
-    for x, y, r in zip(x_coords, y_coords, L):
-        circle = plt.Circle((x, y), r)
-        ax.add_artist(circle)
-
     
     ax.set_aspect('equal', adjustable='datalim')
     ax.set_xlim(x_min, x_max)
     ax.set_ylim(y_min, y_max)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_title('t='+str(t_ind))
 
     # Show the plot
     plt.show()
