6f90d7e983ee8d25b2870b765d28173a17bcbecc
abhmj <146026195+abhmj@users.noreply.github.com>
2023-12-08 11:41:13 -0500

    Comparing dt values

---
+++ functions/dt_compare.py
@@ -0,0 +1,28 @@
+import numpy as np
+import time 
+
+
+def dt_compare(x0, dt, p, f, t_final):
+
+    t_ref = np.arange(0,t_final,dt[-1])
+
+    x_ref = f(x0, t_ref, p)
+
+    error = []
+    t = []
+
+    for step in dt[:-1]:
+        t_vec = np.arange(0,t_final,step)
+
+        t0 = time.time()
+        x = euler(x0, t_vec, p)
+        t1 = time.time()
+
+        t.append(t1-t0)
+
+        error.append(np.linalg.norm(x-x_ref))
+
+    print('Errors for different time discretizations are:',error)
+    print('Time taken for different time discretizations are:',time)
+
+    return error, t
---
+++ functions/simple_trap.py
@@ -0,0 +1,9 @@
+import numpy as np
+from evalf import evalf
+from implicit_trap import trapezoidal
+from jacobian import calc_jac
+
+def simple_trap(x0,t_vec,p):
+    f= lambda x: evalf(x, None,p, None)
+   
+    return trapezoidal(f, x0, t_start=t_vec[0], t_end=t_vec[-1], alpha=5e-3, jf= lambda x: calc_jac(x, p, eps=1e-8), e_f =1e-8, e_delta_x=1e-8, e_x_rel=1e-8, maxiter=100)
