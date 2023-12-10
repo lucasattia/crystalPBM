ea0f619d61e59ea110d354738bc687db653f90b8
abhmj <146026195+abhmj@users.noreply.github.com>
2023-12-10 15:41:24 -0500

    updates to dt_compare

--- functions/dt_compare.py
+++ functions/dt_compare.py
@@ -6,8 +6,12 @@ def dt_compare(x0, dt, p, f, t_final):
 
     t_ref = np.arange(0,t_final,dt[-1])
 
-    x_ref = f(x0, t_ref, p)
+    if (f.__name__ == 'euler' or f.__name__ == 'rk_integrator'):
+        x_ref = f(x0, t_ref, p)
 
+    elif (f.__name__ == 'simple_matrix_free_trap' or f.__name__ == 'simple_trap'):
+        x_ref,_ = f(x0, t_ref, p)
+    
     error = []
     t = []
 
@@ -15,14 +19,19 @@ def dt_compare(x0, dt, p, f, t_final):
         t_vec = np.arange(0,t_final,step)
 
         t0 = time.time()
-        x = f(x0, t_vec, p)
-        t1 = time.time()
 
+        if (f.__name__ == 'euler' or f.__name__ == 'rk_integrator'):
+            x = f(x0, t_ref, p)
+
+        elif (f.__name__ == 'simple_matrix_free_trap' or f.__name__ == 'simple_trap'):
+            x,_ = f(x0, t_ref, p)
+        
+        t1 = time.time()
         t.append(t1-t0)
         indices = np.linspace(0, len(x_ref) - 1, len(x)).astype(int)
         error.append(np.linalg.norm(x-x_ref[indices])/np.linalg.norm(x_ref[indices]))
 
     print('Errors for different time discretizations are:',error)
-    print('Time taken for different time discretizations are:',time)
+    print('Time taken for different time discretizations are:',t)
 
     return error, t
--- functions/simple_trap.py
+++ functions/simple_trap.py
@@ -1,14 +1,14 @@
 import numpy as np
 from evalf import evalf
 from implicit_trap import trapezoidal
+from trapezoidalMatrixFree import trapezoidalMatrixFree
 from jacobian import calc_jac
 
 def simple_trap(x0,t_vec,p):
-    f= lambda x: evalf(x,p)
+    f= lambda x: evalf(x, None,p, None)
    
     return trapezoidal(f, x0, t_start=t_vec[0], t_end=t_vec[-1], alpha=5e-3, jf= lambda x: calc_jac(x, p, eps=1e-8), e_f =1e-8, e_delta_x=1e-8, e_x_rel=1e-8, maxiter=100)
 
 def simple_matrix_free_trap(x0,t_vec,p):
-    f= lambda x: evalf(x,p)
-   
-    return trapezoidal(f, x0, t_start=t_vec[0], t_end=t_vec[-1], alpha=5e-3, jf= lambda x: calc_jac(x, p, eps=1e-8), e_f =1e-8, e_delta_x=1e-8, e_x_rel=1e-8, maxiter=100)
+    alpha= [1e-1, 5e-1, 2]
+    return trapezoidalMatrixFree(evalf,x0, alpha, t_vec[0], T=t_vec[-1], p=p, errf=1e-3, errDeltax=1e-3, relDeltax=1e-3, MaxIter=40,tolrGCR=1e-2,epsMF=1e-6)
